c840e1674571dd8e131c87987d352b5aad54fa9a
Ethan A. G. Baker <ethanagbaker@gmail.com>
2016-05-19 09:57:11 -0400

    comment correction

--- motifDiscovery.sh
+++ motifDiscovery.sh
@@ -9,12 +9,12 @@
 GENOME_DIR="/project/fas/gerstein/eab232/starrseq/data/genomes/Drosphilia/dm3"
 GENOME_FASTA="dm3_forMapping" 
 ANALYSIS_DIR="/project/fas/gerstein/eab232/starrseq/analysis"
-SOFTWARE_DIR="/home/fas/gerstein/eab232/software/bin"
+SOFTWARE_DIR="/home/fas/gerstein/software/bin"
 
 #A tentative motifDiscovery procedure
 
 #Extract sequences for MACS peaks.
-#bedtools getfasta -fi $GENOME_DIR/$GENOME_FASTA.fa -bed $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks_fixedPos.bed -fo $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks.fa
+bedtools getfasta -fi $GENOME_DIR/$GENOME_FASTA.fa -bed $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks_fixedPos.bed -fo $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks.fa
 
 #Use the MEME algorithm for motif discovery.
 $SOFTWARE_DIR/meme -dna -maxsize 7296745 $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks.fa 
