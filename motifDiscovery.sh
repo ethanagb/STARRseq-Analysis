599af505b7ed62978e7fb8d0f766fe4ec63c5a0c
Ethan A. G. Baker <ethanagbaker@gmail.com>
2016-05-19 09:41:08 -0400

    meme

---
+++ motifDiscovery.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#BSUB -q shared
+#BSUB -W 23:55
+#BSUB -J MACS
+#BSUB -R "span[hosts=1]"
+#BSUB -e /project/fas/gerstein/eab232/starrseq/analysis/logs/%J.err
+#BSUB -o /project/fas/gerstein/eab232/starrseq/analysis/logs/%J.out
+
+GENOME_DIR="/project/fas/gerstein/eab232/starrseq/data/genomes/Drosphilia/dm3"
+GENOME_FASTA="dm3_forMapping" 
+ANALYSIS_DIR="/project/fas/gerstein/eab232/starrseq/analysis"
+
+#A tentative motifDiscovery procedure
+
+#Extract sequences for MACS peaks.
+bedtools getfasta -fi $GENOME_DIR/$GENOME_FASTA.fa -bed $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks_fixedPos.bed -fo $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks.fa
+
+#Use the MEME algorithm for motif discovery.
+meme -dna -maxsize 7296745 $ANALYSIS_DIR/S2_STARRseq_rep1_vsControl_peaks.fa 
