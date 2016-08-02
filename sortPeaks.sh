e4f0095d6aba87e1fa87e8b217dcacc84c13c9cd
Ethan Baker <ethanagbaker@gmail.com>
2016-08-02 15:30:20 -0400

    updates

--- sortPeaks.sh
+++ sortPeaks.sh
@@ -6,5 +6,5 @@
 #BSUB -e /project/fas/gerstein/eab232/starrseq/analysis/logs/%J.err
 #BSUB -o /project/fas/gerstein/eab232/starrseq/analysis/logs/%J.out
 
-bedtools intersect -wa -a S2_STARRseq_rep1_vsControl_peaks.bed -b TSS_windows.bed | uniq > S2_STARRseq_rep1_vsControl_peaks_proximal_2kbp.bed
-diff -U $(wc -l < S2_STARRseq_rep1_vsControl_peaks.bed) S2_STARRseq_rep1_vsControl_peaks.bed S2_STARRseq_rep1_vsControl_peaks_proximal_2kbp.bed | grep '^-' | sed 's/^-//g' > S2_STARRseq_rep1_vsControl_peaks_distal_2kbp.bed
+bedtools intersect -wa -a H1WG.SCP1.H1.R1_peaks.bed -b TSS_windows.bed | uniq > H1WG.SCP1.H1.R1_peaks.bed_proximal.bed
+diff -U $(wc -l < H1WG.SCP1.H1.R1_peaks.bed) H1WG.SCP1.H1.R1_peaks.bed H1WG.SCP1.H1.R1_peaks.bed_proximal.bed | grep '^-' | sed 's/^-//g' > S2_STARRseq_rep1_vsControl_peaks_distal_2kbp.bed
